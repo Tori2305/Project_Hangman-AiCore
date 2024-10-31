6dfe1576e9ccda92c44c3ec00d58d1e2ed0d648a
Tori Gilbert <torigilbert@hotmail.com>
2024-10-31 16:07:30 +0000

    Removing uppercase for consistency in code

--- milestone_2.py
+++ milestone_2.py
@@ -3,9 +3,9 @@ fruit_list=["Apple", "Banana", "Orange", "Cherry","Watermelon"]
 
 random_fruit = random.choice(fruit_list)
 
-User_guess = str(input("Please enter a single letter: "))
+user_guess = str(input("Please enter a single letter: "))
 
-if ((User_guess >= "a" and User_guess <="z")) or ((User_guess>="A" and User_guess<="Z")) and len(User_guess) == 1:
+if ((user_guess >= "a" and user_guess <="z")) or ((user_guess>="A" and user_guess<="Z")) and len(user_guess) == 1:
     print("Good guess!")
 else: 
     print("Oops!That is not a valid input.")
