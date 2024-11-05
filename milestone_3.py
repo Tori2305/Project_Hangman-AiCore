198510f4c805cafc1c66e5b717a736ee82e88c57
Tori Gilbert <torigilbert@hotmail.com>
2024-11-05 12:22:23 +0000

    Updating some Refactoring some code to optimise

--- milestone_3.py
+++ milestone_3.py
@@ -5,21 +5,21 @@ fruit_list=["Apple", "Banana", "Orange", "Cherry","Watermelon"]
 random_fruit = random.choice(fruit_list)
 
 
-def check_guess(guess):
-    guess=guess.lower()
-    if guess in random_fruit:
-        print(f"Good guess! {guess} is in the word")
+def check_guess(user_guess):
+    user_guess=user_guess.lower()
+    if user_guess in random_fruit:
+        print(f"Good guess! {user_guess} is in the word")
     else: 
-        print(f"Sorry {guess} not in the word")
+        print(f"Sorry {user_guess} not in the word")
 
 def ask_for_input(): 
     while True:
-        guess = str(input("Please place your guess here: "))
-        if len(guess)!=1 or not guess.isalpha():
+        user_guess = str(input("Please place your guess here: "))
+        if len(user_guess)!=1 or not user_guess.isalpha():
             print("Invalid letter. Please, enter a single alphabetical character: ")
         else: 
            break
-    check_guess(guess)
+    check_guess(user_guess)
 
         
 ask_for_input()
