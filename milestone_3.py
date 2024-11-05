1b72eef281dbe8fba91a3031a5ccf4e30e70a7f2
Tori Gilbert <torigilbert@hotmail.com>
2024-11-05 12:19:45 +0000

    Asking user for input and responding as to whether letter is within random fruit selected

--- milestone_3.py
+++ milestone_3.py
@@ -3,18 +3,23 @@ import random
 fruit_list=["Apple", "Banana", "Orange", "Cherry","Watermelon"]
 
 random_fruit = random.choice(fruit_list)
-while True:
-    guess=input("Enter a single character here: ")
-    if guess.isalpha() and len(guess)==1:
-        break
-    else: 
-        print("Invalid letter. Please, enter a single alphabetical character: ")
 
-for fruit in random_fruit:
-    if guess in fruit:
+
+def check_guess(guess):
+    guess=guess.lower()
+    if guess in random_fruit:
         print(f"Good guess! {guess} is in the word")
     else: 
         print(f"Sorry {guess} not in the word")
-        break
 
-print(random_fruit)
+def ask_for_input(): 
+    while True:
+        guess = str(input("Please place your guess here: "))
+        if len(guess)!=1 or not guess.isalpha():
+            print("Invalid letter. Please, enter a single alphabetical character: ")
+        else: 
+           break
+    check_guess(guess)
+
+        
+ask_for_input()
