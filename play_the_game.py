6bf08e00ac2d5a1c3e2400bf55adb35c8c8a7d4c
Tori Gilbert <torigilbert@hotmail.com>
2024-11-12 09:51:46 +0000

    Update play_the_game.py
    Updating for PEP8

--- play_the_game.py
+++ play_the_game.py
@@ -26,11 +26,11 @@ class Hangman:
         if correct or incorrect
       """
     self.word_list = word_list
-    self.num_lives= num_lives
-    self.word= random.choice(self.word_list)
-    self.word_guessed=[" _ "] * len(self.word)
-    self.num_letters=len(set(self.word))
-    self.list_of_guesses=[]
+    self.num_lives = num_lives
+    self.word = random.choice(self.word_list)
+    self.word_guessed = [" _ "] * len(self.word)
+    self.num_letters = len(set(self.word))
+    self.list_of_guesses = []
 
   def check_guess(self,guess):
     """
@@ -42,7 +42,7 @@ class Hangman:
     if guess in self.word:
         print(f"Good guess! {guess} is in the word")
         for index,char in enumerate(self.word):
-           if self.word[index]==guess:
+           if self.word[index] == guess:
               self.word_guessed[index] = guess
         self.num_letters -= 1
         print(" ".join(self.word_guessed))
@@ -59,8 +59,8 @@ class Hangman:
     and not been guessed before it then runs check_guess().
     """
     while True:
-      guess=input("Please guess a single alphabetical letter: ")
-      guess=guess.lower()
+      guess = input("Please guess a single alphabetical letter: ")
+      guess = guess.lower()
       if len(guess) != 1 or not guess.isalpha():
         print("Invalid letter. Please, enter a single alphabetical character: ")
       elif guess in self.list_of_guesses:
@@ -77,8 +77,8 @@ def play_game(word_list):
   It also provides feedback as to when the user has won/lost the game. They can win if
   all the letters are guessed correctly or lose if they run out of lives.
   """
-  num_lives=5
-  game=Hangman(word_list, num_lives)
+  num_lives = 5
+  game = Hangman(word_list, num_lives)
   while True:
     if game.num_lives == 0:
       print("You've lost!")
@@ -90,7 +90,7 @@ def play_game(word_list):
       print("Congratulations. You won the game!")
       break
 
-if __name__ =="__main__":
+if __name__ == "__main__":
   fruit_list = ["apple", "banana", "orange", "grape", "watermelon"]
   play_game(fruit_list)
 
